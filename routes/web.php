63ec9c1ab17318c93bdccd215ead719bbc7272d9
Aixa28 <pilar.fuentes2808@gmail.com>
2026-09-14 16:23:40 -0300

    Guardando cambios de login y rutas antes del pull

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Mostrar formulario de registro
+// Rutas de Registro
 Route::get('/registro', [RegisterController::class, 'create'])->name('register');
-
-// Guardar datos del usuario
 Route::post('/registro', [RegisterController::class, 'store']);
+
+// Rutas de Login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
