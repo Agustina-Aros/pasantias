92610971a4d62674ee421a58f6660386c5b700c6
Ceferino <arl672504@gmail.com>
2026-09-14 15:47:21 -0300

    Creando diseño de login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('verificar.login');
+});
+
+Route::post('/login', function (Request $request) {
+    $nombre = $request->input('nombre');
+    $email = $request->input('email');
+    $passwordEncriptada = Hash::make($request->input('password'));
+
+    return "Formulario recibido correctamente.<br>Usuario: " . $nombre;
 });
