27f8d70d6c7accb46753ae0411b88ff547e482cc
Sebastián Illanes <sebastianillanesroojass@gmail.com>
2026-09-14 16:15:25 -0300

    Login (blades, web.php y controlador)

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,55 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\RegisterController;
+
 
 Route::get('/', function () {
-    return view('welcome');
-
-    use App\Http\Controllers\RegisterController;
-
-// Mostrar formulario
-Route::get('/registro', [RegisterController::class, 'create'])->name('register');
-
-// Guardar usuario
-Route::post('/registro', [RegisterController::class, 'store']);
+    return redirect()->route('login');
 });
+
+Route::get('/registro', [RegisterController::class, 'create'])->name('register');
+Route::post('/registro', [RegisterController::class, 'store']);
 
 Route::get('/login', function () {
     return view('verificar.login');
-});
+})->name('login');
+
 
 Route::post('/login', function (Request $request) {
-    $nombre = $request->input('nombre');
-    $email = $request->input('email');
-    $passwordEncriptada = Hash::make($request->input('password'));
+    
+    $credentials = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
 
-    return "Formulario recibido correctamente.<br>Usuario: " . $nombre;
+    if (Auth::attempt($credentials)) {
+        $request->session()->regenerate();
+        return redirect()->intended('/dashboard'); 
+    }
+
+    return back()->withErrors([
+        'email' => 'Las credenciales no coinciden con nuestros registros.',
+    ]);
+});
+
+
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::post('/logout', function (Request $request) {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    })->name('logout');
+
 });
