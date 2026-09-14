45b9407546cd2862bcc0cac9876bce4317294997
Aixa28 <pilar.fuentes2808@gmail.com>
2026-09-14 15:59:40 -0300

    Guardando avances locales antes de actualizar repositorios

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
-
-    use App\Http\Controllers\RegisterController;
+});
 
 // Mostrar formulario
 Route::get('/registro', [RegisterController::class, 'create'])->name('register');
 
 // Guardar usuario
 Route::post('/registro', [RegisterController::class, 'store']);
-});
