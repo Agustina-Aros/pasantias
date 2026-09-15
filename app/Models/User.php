1410b35cf92c8fa96068a53f7f71cc218bebd5f3
Agustina-Aros <agustinaaros38@gmail.com>
2026-09-15 14:21:20 -0300

    Modificando  el modelo de usuario

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,34 +1,28 @@
 <?php
+
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    use HasFactory, Notifiable;
+
     protected $table = 'usuarios';
 
     protected $fillable = [
-        'nombre',
+        'nombre', 
         'email',
         'password',
     ];
-}
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
