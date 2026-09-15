2fe0c3a0f014954b4885f8b755ffccd96a605184
Agustina-Aros <agustinaaros38@gmail.com>
2026-09-15 14:18:58 -0300

    Modificando tabla de usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,28 +1,18 @@
 <?php
-
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    protected $table = 'usuarios';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
-        'name',
+        'nombre',
         'email',
         'password',
     ];
+}
 
     /**
      * The attributes that should be hidden for serialization.
