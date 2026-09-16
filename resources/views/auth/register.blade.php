b44703968872fe966802d07a533d3b9379a7032a
Ceferino <arl672504@gmail.com>
2026-09-16 16:10:30 -0300

    Añadiendo logo como icono de todo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de Usuario</title>
+    <link rel="icon" type="image/png" href="{{ asset('imagenes/icono.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light d-flex align-items-center justify-content-center vh-100">
