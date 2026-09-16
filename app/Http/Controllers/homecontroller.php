f02fec68ecfc0841cfa44bf3383dad25448ef80f
Agustina-Aros <agustinaaros38@gmail.com>
2026-09-16 15:10:32 -0300

    Arreglando los controladores para la paginación

--- app/Http/Controllers/homecontroller.php
+++ app/Http/Controllers/homecontroller.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $productos = Producto::all();
+        $productos = Producto::paginate(5);
 
         return view('home', compact('productos'));
     }
